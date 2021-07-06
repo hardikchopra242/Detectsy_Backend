70315be225a9fec4213e62bda28e6bc2279ce1bc
Hardik Chopra <hardikchopra242@gmail.com>
2021-07-06 19:34:47 +0530

    [Lint] removed comments

--- controllers/image.js
+++ controllers/image.js
@@ -15,7 +15,7 @@ const handleApiCall = (req, res) => {
 }
 
 
-const handleImage =  (req,res,db)=>{    					//fot the time being we are using 'image' in place of a url
+const handleImage =  (req,res,db)=>{   
 	const { id } = req.body;
 	db('users').where('id','=', id)
 	.increment('entries', 1)
